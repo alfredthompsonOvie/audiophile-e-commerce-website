cc1be0f353fc2ad5e8e0f4a11389291185fca5d0
thompson <alfredthompsonovie@gmail.com>
2023-03-08 01:59:58 +0100

    fixed mobile nav animation

--- src/components/TheNavBAr.vue
+++ src/components/TheNavBAr.vue
@@ -3,18 +3,10 @@
 		<section class="grid__content nav__content">
 			<!-- hamburger -->
 			<section class="hamburger" v-if="isMobile">
-				<button 
-        class="openMenu" 
-        v-if="!isMenuOpen"
-        @click="isMenuOpen = !isMenuOpen"
-        >
+				<button class="openMenu" v-if="!isMenuOpen" @click="handleClick">
 					<font-awesome-icon icon="fa-solid fa-bars" />
 				</button>
-				<button 
-        class="closeMenu" 
-        v-if="isMenuOpen"
-        @click="isMenuOpen = !isMenuOpen"
-        >
+				<button class="closeMenu" v-if="isMenuOpen" @click="handleClick">
 					<font-awesome-icon icon="fa-solid fa-xmark" />
 				</button>
 			</section>
@@ -25,44 +17,39 @@
 
 			<!-- menu -->
 			<!-- mobile  -->
-      <transition
-      :css="false"
-      @enter="onEnter"
-      @leave="onLeave"
-      >
-        <ul class="dropdown" v-show="isMenuOpen">
-          <li class="dropdown__item">
-            <router-link :to="{ name: 'product' }" class="dropdown__link">
-              <img src="@/assets/images/image-headphone-menu.svg" alt="" />
-              <p>Headphones</p>
-              <span>
-                <span class="span__text"> shop </span>
-                <font-awesome-icon icon="fa-solid fa-angle-right" />
-              </span>
-            </router-link>
-          </li>
-          <li class="dropdown__item">
-            <router-link :to="{ name: 'product' }" class="dropdown__link">
-              <img src="@/assets/images/image-speakers-menu.svg" alt="" />
-              <p>Speakers</p>
-              <span>
-                <span class="span__text"> shop </span>
-                <font-awesome-icon icon="fa-solid fa-angle-right" />
-              </span>
-            </router-link>
-          </li>
-          <li class="dropdown__item">
-            <router-link :to="{ name: 'product' }" class="dropdown__link">
-              <img src="@/assets/images/image-earphone-menu.png" alt="" />
-              <p>Earphones</p>
-              <span>
-                <span class="span__text"> shop </span>
-                <font-awesome-icon icon="fa-solid fa-angle-right" />
-              </span>
-            </router-link>
-          </li>
-        </ul>
-      </transition>
+			<ul class="dropdown" v-show="isMenuOpen">
+				<li class="dropdown__item menuItem">
+					<router-link :to="{ name: 'product' }" class="dropdown__link">
+						<img src="@/assets/images/image-headphone-menu.svg" alt="" />
+						<p>Headphones</p>
+						<span>
+							<span class="span__text"> shop </span>
+							<font-awesome-icon icon="fa-solid fa-angle-right" />
+						</span>
+					</router-link>
+				</li>
+				<li class="dropdown__item menuItem">
+					<router-link :to="{ name: 'product' }" class="dropdown__link">
+						<img src="@/assets/images/image-speakers-menu.svg" alt="" />
+						<p>Speakers</p>
+						<span>
+							<span class="span__text"> shop </span>
+							<font-awesome-icon icon="fa-solid fa-angle-right" />
+						</span>
+					</router-link>
+				</li>
+				<li class="dropdown__item menuItem">
+					<router-link :to="{ name: 'product' }" class="dropdown__link">
+						<img src="@/assets/images/image-earphone-menu.png" alt="" />
+						<p>Earphones</p>
+						<span>
+							<span class="span__text"> shop </span>
+							<font-awesome-icon icon="fa-solid fa-angle-right" />
+						</span>
+					</router-link>
+				</li>
+			</ul>
+
 			<!-- desktop nav -->
 			<ul class="nav__menu" v-if="!isMobile">
 				<li>
@@ -107,46 +94,30 @@ const checkScreen = () => {
 	isMenuOpen.value = false;
 	return;
 };
+
+const tl = gsap.timeline({
+	reversed: true,
+	onStart: () => {
+		isMenuOpen.value = !isMenuOpen.value;
+	},
+	onReverseComplete: () => {
+		isMenuOpen.value = !isMenuOpen.value;
+	},
+});
 onMounted(() => {
-  checkScreen();
-  window.addEventListener("resize", checkScreen)
-})
-const tl = gsap.timeline()
+	checkScreen();
+	window.addEventListener("resize", checkScreen);
 
-const onEnter = (el, done) => {
-  el.addEventListener('transitionEnd', () => {
-    console.log('end');
-    
-  })
-  tl
-    .from(el.children, {
-      autoAlpha:0.01,
-      x: -50,
-      stagger: 0.3,
-      onComplete: () => {
-        gsap.from(el.children, {
-          clearProps: true
-        })
-        done();
-      },
-    })
-  
-}
-const onLeave = (el, done) => {
-  tl
-    .to(el.children, {
-      y: -50,
-      stagger: 0.1,
-      autoAlpha: 0.01,
-      onComplete: () => {
-        gsap.from(el.children, {
-          clearProps: true
-        })
-        done();
-      },
-    })
-}
+	tl.from(".menuItem", {
+		autoAlpha: 0.01,
+		x: -50,
+		stagger: 0.3,
+	});
+});
 
+const handleClick = () => {
+	tl.reversed(!tl.reversed());
+};
 </script>
 
 <style scoped>
@@ -173,83 +144,34 @@ nav {
 	background-color: #fff;
 	z-index: 20;
 }
-.dropdown__item {
-	background: #f1f1f1;
-	border-radius: 8px;
-}
-.dropdown__item + .dropdown__item {
-	margin-top: 4em;
-}
-.dropdown__link {
-	position: relative;
-	display: block;
-	text-align: center;
-	padding: 5em 2em 2em;
-	font-size: 15px;
-}
-.dropdown__link img {
-	position: absolute;
-	top: -50px;
-	left: 50%;
-	transform: translateX(-50%);
-}
-.dropdown__link p {
-	color: #000;
-	text-transform: uppercase;
-	font-weight: 700;
-	letter-spacing: 1.07143px;
-	line-height: 20px;
-	margin-bottom: 0.7em;
-}
-.dropdown__link span {
-	display: inline-flex;
-	align-items: center;
-	gap: 0.5em;
-}
-.span__text {
-	letter-spacing: 1px;
-	text-transform: uppercase;
-	color: #000000;
-	mix-blend-mode: normal;
-	opacity: 0.5;
-	font-weight: 700;
-	font-size: 13px;
-}
-.fa-angle-right {
-	color: #d87d4a;
-}
-/* color: #D87D4A;
- */
+
 .nav__menu {
-  display: flex;
-  align-items: center;
-  gap: 2em;
+	display: flex;
+	align-items: center;
+	gap: 2em;
 }
 .nav__menu a {
-  font-size: 13px;
-  /* font-weight: 700; */
-line-height: 25px;
-letter-spacing: 2px;
-/* transition: all .3s linear; */
-position: relative;
+	font-size: 13px;
+	line-height: 25px;
+	letter-spacing: 2px;
+	position: relative;
 }
 
 .nav__menu a::before {
-  content: "";
-  position: absolute;
-  width: 0%;
-  height: 150%;
-  left: 50%;
-  transition: all .3s linear;
-  background-color: #D87D4A;
-  mix-blend-mode: multiply;
+	content: "";
+	position: absolute;
+	width: 0%;
+	height: 150%;
+	left: 50%;
+	transition: all 0.3s linear;
+	background-color: #d87d4a;
+	mix-blend-mode: multiply;
 }
 
 .nav__menu a:hover::before {
-  left: 0%;
-  width: 100%;
+	left: 0%;
+	width: 100%;
 }
-
 
 @media (min-width: 600px) and (max-width: 991px) {
 	.nav__content {
